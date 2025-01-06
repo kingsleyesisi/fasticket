099af2873ad40462d84577a9c22a0c92ff6c23fc
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2025-01-06 20:07:31 +0100

    Update serializers.py

--- fasticket_app/serializers.py
+++ fasticket_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import UserProfile, EventsInfo
+from .models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
