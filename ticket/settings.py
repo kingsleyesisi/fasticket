acb6143d2ac8ec95dfec92134c4a8fcb7b2736ab
KINGSLEY ESISI <138278444+kingsleyesisi@users.noreply.github.com>
2025-01-19 04:46:12 +0100

    added allowed origin for the frontend

--- ticket/settings.py
+++ ticket/settings.py
@@ -59,6 +59,10 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ticket.urls'
 
+
+
+
+# NOTE this is not recommended for production just for debugging and testing during development
 # CORS Settings
 CORS_ALLOW_ALL = True
 
@@ -78,6 +82,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://fasticket-react-w1x7.vercel.app',
     'http://localhost:8000',
+    'https://fasticketss.vercel.app',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
@@ -89,7 +94,6 @@ CORS_ALLOW_PRIVATE = True
 
 CORS_ALLOW_PROXY = True
 
-# NOTE this is not recommended for production just for debugging the 
 
 TEMPLATES = [
     {
