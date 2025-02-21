3fdf1c1acf5e6d4578462bebe6b9de104803d53d
kingsleyesisi <kingsleyesisi@outlook.com>
2025-02-21 03:10:21 +0100

    Register Events and Tickets models in admin; clean up URL patterns by removing commented paths

--- Event_Manager/admin.py
+++ Event_Manager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Events, Tickets
 
-# Register your models here.
+admin.site.register(Events)
+# admin.site.register(TicketCategory)
+admin.site.register(Tickets)
--- Event_Manager/urls.py
+++ Event_Manager/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('get', views.CreateEventView.as_view(), name='get'),
     path('create', views.CreateEvent.as_view(), name='create'),
     path('update/<int:pk>', views.UpdateEventView.as_view(), name='update'),
     # path('delete', views.DeleteEvent.as_view(), name='delete'),
