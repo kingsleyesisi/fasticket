4d464423636f76b0378045a88eb9458434521148
kingsleyesisi <kingsleyesisi@outlook.com>
2025-02-21 01:44:29 +0100

    Add event creation view and form template; update URL patterns and permissions to allow any user get events

--- Event_Manager/urls.py
+++ Event_Manager/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get', views.CreateEventView.as_view(), name='get'),
+    # path('get', views.CreateEventView.as_view(), name='get'),
     path('create', views.CreateEvent.as_view(), name='create'),
+    path('view', views.CreateView, name='view'),
     path('get', views.GetEvent.as_view(), name='list'),
 ]
--- Event_Manager/views.py
+++ Event_Manager/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import permission_classes
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import HttpResponse
-from .serializers import EventSerializer, TicketSerializer
-from .models import Events, Tickets
+from .serializers import EventSerializer
+from .models import Events
 
 
 
@@ -15,7 +15,7 @@ class CreateEventView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
         
-    def get(request, *args, **kwagr):
+    def get(self, request):
         try:
             if permission_classes:
                 return Response({'M':"it i authentiated"}, status=200)
@@ -23,14 +23,14 @@ class CreateEventView(APIView):
                 return HttpResponse("user")
         except Exception as e:
             return Response(e)
-    def post(request, *arg, **kwagr):
-        yield HttpResponse('This is the post request')
+    def post(self, request):
+        return HttpResponse('This is the post request')
 
-    def put(request, *args, **kwargs):
-        pass
+    def put(self, request):
+        return HttpResponse('This is a put request')
         return HttpResponse('This is a put request')
     
-    def delete(request, *args, **kwargs):
+    def delete(self, request):
         return HttpResponse('this is a delete request')
 
 
@@ -41,23 +41,37 @@ class CreateEvent(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, format=None):
-        serializer = EventSerializer(data=request.data)
+    def post(self, request):
+        user = request.user
+        user_pk = user.pk
         
+        data = request.data.copy()
+        data['user'] = user_pk
+
+        serializer = EventSerializer(data=data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(
                 {"message": "Event created successfully!", "data": serializer.data},
                 status=status.HTTP_201_CREATED
             )
-        
+        print(serializer.errors)
+        print(type(user_pk))
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 class GetEvent(APIView):
     """
     Get all events.
     """
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
     def get(self, request, format=None):
         events = Events.objects.all()
         serializer = EventSerializer(events, many=True)
         return Response(serializer.data)
-    
+
+def CreateView(request):
+    pass 
+    return render(request, 'form.html', status=200)
