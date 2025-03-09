3fdea99a44d8fa0fca29e9cdbaf53835571b43cf
kingsleyesisi <kingsleyesisi@outlook.com>
2025-03-09 20:35:14 +0100

    Fixed the bug for creation of event ticket

--- Event_Manager/serializers.py
+++ Event_Manager/serializers.py
@@ -15,13 +15,29 @@ class EventSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate(self, attrs):
+        """
+        Validate the data.
+
+        This method checks if the event is marked as paid and ensures that at least one ticket is included.
+        If the event is paid and no tickets are provided, a ValidationError is raised.
+
+        Args:
+            attrs (dict): The attributes to validate.
+
+        Returns:
+            dict: The validated attributes, including the tickets.
+
+        Raises:
+            serializers.ValidationError: If the event is paid and no tickets are provided.
+        """
+
         is_paid = attrs.get('is_paid', False)
         tickets = self.initial_data.get('tickets')
-        if is_paid and (tickets is False or len(tickets) == 0):
+        if is_paid and (tickets is None or len(tickets) == 0):
             raise serializers.ValidationError("Paid events must include at least one ticket.")
 
+        attrs['tickets'] = tickets
         return attrs
-
 
     def create(self, validated_data):
         tickets_data = validated_data.pop('tickets', [])  # Extract ticket
