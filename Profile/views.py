9d2eae69dbd0e22260b6bbbdd6b1d6f2f65bf568
kingsleyesisi <kingsleyesisi@outlook.com>
2025-07-15 01:50:08 +0100

    added jwt import and also sqlite to the repo

--- Profile/views.py
+++ Profile/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
